7345338a78048626a2fbc43b2337168667988348
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-04-28 15:06:58 +0700

    ToDo ListTile Demo1

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -5,6 +5,18 @@ class ToDoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: EdgeInsets.all(20),
+      child: ListTile(
+        leading: const Text("ToDo", style: TextStyle(fontSize: 20)),
+        onTap: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: const Color.fromRGBO(204, 119, 34, 100),
+        trailing: const Icon(Icons.delete,color: Colors.red,),
+      ),
+    );
   }
 }
