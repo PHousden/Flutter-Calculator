cd23d6d7d35f8d1c25c4e8da716e022082c670a2
philip.houden.04@gmail.com <philip.houden.04@gmail.com>
2023-04-28 13:06:18 +0700

    Getting the debug banner off & Home page

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/HomePage.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -6,7 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
+        //Getting the Debug banner off
+      debugShowCheckedModeBanner: false,
+      // Home Page
+      home: HomePage(),
     );
   }
 }
